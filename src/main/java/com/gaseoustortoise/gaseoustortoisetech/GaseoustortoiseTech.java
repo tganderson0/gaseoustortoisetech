a6d5d8c183913d41413013081dcc7d4e32abb93c
Taylor Anderson <tganderson0@gmail.com>
2024-09-29 19:33:31 -0600

    Create first items

--- src/main/java/com/gaseoustortoise/gaseoustortoisetech/GaseoustortoiseTech.java
+++ src/main/java/com/gaseoustortoise/gaseoustortoisetech/GaseoustortoiseTech.java
@@ -1,5 +1,7 @@
 package com.gaseoustortoise.gaseoustortoisetech;
 
+import com.gaseoustortoise.gaseoustortoisetech.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -38,6 +40,8 @@ public class GaseoustortoiseTech
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -52,6 +56,10 @@ public class GaseoustortoiseTech
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
