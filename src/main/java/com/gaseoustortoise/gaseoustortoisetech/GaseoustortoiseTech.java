8e4328929d74a53fb96185581c7eeffc379b3481
Taylor Anderson <tganderson0@gmail.com>
2024-09-29 20:11:29 -0600

    Create creative mode tabs

--- src/main/java/com/gaseoustortoise/gaseoustortoisetech/GaseoustortoiseTech.java
+++ src/main/java/com/gaseoustortoise/gaseoustortoisetech/GaseoustortoiseTech.java
@@ -1,6 +1,7 @@
 package com.gaseoustortoise.gaseoustortoisetech;
 
 import com.gaseoustortoise.gaseoustortoisetech.block.ModBlocks;
+import com.gaseoustortoise.gaseoustortoisetech.item.ModCreativeModeTabs;
 import com.gaseoustortoise.gaseoustortoisetech.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -40,6 +41,8 @@ public class GaseoustortoiseTech
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
---
+++ src/main/java/com/gaseoustortoise/gaseoustortoisetech/item/ModCreativeModeTabs.java
@@ -0,0 +1,42 @@
+package com.gaseoustortoise.gaseoustortoisetech.item;
+
+import com.gaseoustortoise.gaseoustortoisetech.GaseoustortoiseTech;
+import com.gaseoustortoise.gaseoustortoisetech.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, GaseoustortoiseTech.MOD_ID);
+
+    public static void register(IEventBus eventBus){
+        CREATIVE_MODE_TAB.register(eventBus);
+    }
+
+    public static final Supplier<CreativeModeTab> BISMUTH_ITEMS_TAB = CREATIVE_MODE_TAB.register("bismuth_items_tab",
+            () -> CreativeModeTab.builder()
+            .icon(() -> new ItemStack(ModItems.BISMUTH.get()))
+                    .title(Component.translatable("creativetab.gaseoustortoisetech.bismuth_items"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.BISMUTH);
+                        output.accept(ModItems.RAW_BISMUTH);
+                    })
+            .build());
+
+    public static final Supplier<CreativeModeTab> BISMUTH_BLOCK_TAB = CREATIVE_MODE_TAB.register("bismuth_blocks_tab",
+            () -> CreativeModeTab.builder()
+            .icon(() -> new ItemStack(ModBlocks.BISMUTH_BLOCK.get()))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath(GaseoustortoiseTech.MOD_ID, "bismuth_items_tab"))
+                    .title(Component.translatable("creativetab.gaseoustortoisetech.bismuth_blocks"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModBlocks.BISMUTH_BLOCK);
+                        output.accept(ModBlocks.BISMUTH_ORE);
+                    })
+            .build());
+}
